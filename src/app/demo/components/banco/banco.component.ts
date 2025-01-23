d94a42af62aa666510a3592f680a9fc72c26d62d
User <User@LAPTOP-UGSJCCHL.com>
2025-01-23 14:45:48 -0500

    unión banco y cuenta bancaria

--- src/app/demo/components/banco/banco.component.ts
+++ src/app/demo/components/banco/banco.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { CardModule } from 'primeng/card';
+import { Component, OnInit } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { TableModule } from 'primeng/table';
 import { InputTextModule } from 'primeng/inputtext';
@@ -10,180 +11,192 @@ import { PanelModule } from 'primeng/panel';
 import { ConfirmDialogModule } from 'primeng/confirmdialog';
 import { MensajemodalComponent } from "../mensajemodal/mensajemodal.component";
 import { CommonModule } from '@angular/common';
-import { ConfirmationService } from 'primeng/api';
+import { Router } from '@angular/router';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { BreadcrumbService } from '../../service/breadcrumb.service';
+import { RouterLink } from '@angular/router';
+import { ToastModule } from 'primeng/toast';
+import { BreadcrumbModule } from 'primeng/breadcrumb';
 
 
 @Component({
   selector: 'app-banco',
   standalone: true,
-  imports: [ButtonModule, TableModule, InputTextModule, DialogModule,ReactiveFormsModule, FormsModule,
-    PanelModule, ConfirmDialogModule, MensajemodalComponent,CommonModule],
+  imports: [ToastModule, TableModule, ReactiveFormsModule, CommonModule, ButtonModule, CardModule, InputTextModule, PanelModule, BreadcrumbModule, ConfirmDialogModule, FormsModule],
   templateUrl: './banco.component.html',
   styleUrl: './banco.component.css',
-  providers:[ConfirmationService]
+  providers: [MessageService, ConfirmationService]
 })
-export class BancoComponent {
-  bancos:Banco[]=[];
-  originalBancos:Banco[]=[];
-  selectedBanco:Banco| null = null;
-  formBanco: FormGroup;
-  visible:boolean=false;
-  mostrarMensajeModal:boolean = false;
-  mensajeModal:string='';
-  displayDialog:boolean=false;
+export class BancoComponent implements OnInit {
+  bancoForm: FormGroup;
+  bancoList: Banco[]
+  isEditing: boolean = false;
+  editingRowIndex: number | null = null;
+  editingBanco: Banco | null = null;
+  editingRows: { [s: string]: boolean } = {};
+  editingData: any = {};
+  displayDialog: boolean = false;
+  isNew: boolean = false;
+  clonedBancos: { [s: string]: Banco } = {}
+  items: any[] = [];
+  isEditingAnyRow: boolean = false;
 
-  constructor(private bancoService: BancoService, private fb:FormBuilder){
-     //bancoService.GetBancos().subscribe(res => {this.bancoList = res; console.log(this.bancoList); }, error => console.log(error));
-        this.formBanco=fb.group({
-          ban01Empresa: ['', Validators.required],
-          ban01IdBanco: ['', Validators.required],
-          ban01Descripcion: ['', Validators.required],
-          ban01Prefijo: ['', Validators.required]
-        });
+  // bancos: Banco[] = [];
+  // originalBancos: Banco[] = [];
+  // selectedBanco: Banco | null = null;
+  // formBanco: FormGroup;
+  // visible: boolean = false;
+  // mostrarMensajeModal: boolean = false;
+  // mensajeModal: string = '';
+  // displayDialog: boolean = false;
+  // items: any[] = [];
+  // currentBancoDescripcion: string;
+  // banco: Banco[] = [];
+
+  constructor(private bancoService: BancoService, private fb: FormBuilder, private mS: MessageService, private confirmationService: ConfirmationService, private bS: BreadcrumbService, private router: Router) {
+  }
+  // ngOnInit(): void {
+  //   this.bS.setBreadcrumbs([
+  //     { icon: 'pi pi-home', routerLink: '/' },
+  //     { label: 'Banco', routerLink: '/bancos' }
+  //   ]);
+  //   this.bS.currentBreadcrumbs$.subscribe(bc => {
+  //     this.items = bc;
+  //   })
+  //   this.llenarData();
+  // };
+  ngOnInit(): void {
+    this.bS.setBreadcrumbs([
+      { icon: 'pi pi-home', routerLink: '/Home' },
+      { label: 'Bancos', routerLink: '/Home/banco' }
+    ]);
+    this.bS.currentBreadcrumbs$.subscribe(bc => {
+      this.items = bc;
+    })
+    this.initForm()
+    this.cargarBancos()
+  }
+  // Función para navegar a las cuentas del banco seleccionado
+  verCuentas(banco): void {
+    // Primero, actualiza las migas de pan antes de navegar
+    this.bS.setBreadcrumbs([
+      { icon: 'pi pi-home', routerLink: '/' },
+      { label: 'Banco', routerLink: '/Home/banco' },
+      { label: 'Ver Cuentas' }
+    ]);
+    this.bS.currentBreadcrumbs$.subscribe(bc => {
+      this.items = bc;
+    })
+    this.router.navigate(['/Home/banco/cuentas'], {
+      queryParams: {
+        idBanco: banco.ban01IdBanco,
+        descripcion: banco.ban01Descripcion
+      }
+    });
+    // Luego, navega a la ruta correspondiente de "Ver Cuentas"
+    //this.router.navigate([`/Home/banco/${banco.ban01IdBanco}/cuentas/${banco.ban01Descripcion}`]);
+  }
+  initForm() {
+    this.bancoForm = this.fb.group({
+      ban01Empresa: ['01', Validators.required],
+      ban01IdBanco: ['', Validators.required],
+      ban01Descripcion: ['', Validators.required],
+      ban01Prefijo: ['', Validators.required]
+    })
   }
 
-  ngOnInit(): void {
-    this.llenarData();
-   };
-
-  llenarData() {
-    this.bancoService.GetBancos().subscribe(bancos => {
-      this.bancos = bancos;
-      this.originalBancos=this.bancos
-    }, error => {
-      this.mostrarMensaje('Error al cargar bancos.');
-      console.error(error);
+  // cargar data
+  cargarBancos(): void {
+    this.bancoService.GetBancos().subscribe({
+      next: (data) => this.bancoList = data,
+      error: (error) => {
+        this.mS.add({
+          severity: 'error',
+          summary: 'Error',
+          detail: 'Error al cargar bancos'
+        });
+      }
     });
   }
-
-
-  guardar() {
-    if (this.formBanco.invalid) {
-      this.formBanco.markAllAsTouched(); // Marca todos los campos como tocados
-      this.mostrarMensaje('Por favor, complete todos los campos requeridos.');
-      return;
-    }
-
-    if (this.selectedBanco) {
-      // Actualización
-      this.bancoService.ActualizarBanco(        
-        this.formBanco.value
-      ).subscribe(
-        response => {
-          
-          this.mostrarMensaje('El Banco se ha actualizado correctamente.');
-          this.llenarData();
-          this.resetFormulario();
+  //edicion
+  onRowEditInit(banco: Banco): void {
+    this.editingBanco = { ...banco }
+    this.isEditingAnyRow = true;
+  }
+  onRowEditSave(banco: Banco): void {
+    if (this.editingBanco) {
+      this.bancoService.ActualizarBanco(banco).subscribe({
+        next: () => {
+          this.editingBanco = null;
+          this.isEditingAnyRow = false;
+          this.mS.add({ severity: 'success', summary: 'Éxito', detail: 'Registro actualizado' });
         },
-        error => {
-          this.mostrarMensaje('Error al actualizar banco.');
-          console.error('Error al actualizar banco', error);
+        error: () => {
+          this.mS.add({ severity: 'error', summary: 'Error', detail: 'Error al actualizar' });
         }
-      );
-    } else {
-      // Creación
-      this.bancoService.CrearBanco(this.formBanco.value).subscribe(
-        response => {
-          this.mostrarMensaje('El Banco se ha creado correctamente.');
-          this.llenarData();
-          this.resetFormulario();
+      })
+    }
+  }
+  onRowEditCancel(banco: Banco, index: number): void {
+    if (this.editingBanco) {
+      this.bancoList[index] = { ...this.editingBanco };
+      this.editingBanco = null;
+      this.isEditingAnyRow = false;
+      this.cargarBancos();
+    }
+  }
+  //crear banco
+  showAddRow() {
+    this.isEditing = true;
+    this.isNew = true;
+    this.bancoForm.reset({ ban01Empresa: '01' });
+  }
+  onSave() {
+    if (this.bancoForm.valid) {
+      const newBanco: Banco = this.bancoForm.value;
+      this.bancoService.CrearBanco(newBanco).subscribe({
+        next: () => {
+          this.isEditing = false;
+          this.isNew = false;
+          this.bancoForm.reset();
+          this.mS.add({ severity: 'success', summary: 'Éxito', detail: 'Registro guardado' });
+          this.cargarBancos();
         },
-        error => {
-          this.mostrarMensaje('Error al crear banco.');
-          console.error('Error al crear banco', error);
-        }
-    }
-
-    this.visible = false; // Cerrar el modal
-  }
-
-  // eliminar
-    confirmarEliminacion(banco:Banco){
-        this.selectedBanco=banco;
-        this.displayDialog=true;
-    }
-
-    cancelarEliminacion() {
-        this.displayDialog = false; // Cerrar el diálogo sin hacer nada
-    }
-
-    eliminarBanco() {
-        if (this.selectedBanco) {
-            this.bancoService.EliminarBanco(this.selectedBanco.ban01Empresa, this.selectedBanco.ban01IdBanco).subscribe(
-                () => {
-                    this.mostrarMensaje(`${this.selectedBanco.ban01Descripcion} ha sido eliminado correctamente.`);
-                    this.llenarData(); // Actualizar los datos
-                    this.displayDialog = false; // Cerrar el diálogo
-                },
-                error => {
-                    if (error.error && error.error.message) {
-                        this.mostrarMensaje(error.error.message); // Mostrar el mensaje de error
-                    } else {
-                        this.mostrarMensaje('Error al eliminar banco');
-                    }
-                    console.error('Error al eliminar banco', error);
-                    this.displayDialog = false; // Cerrar el diálogo en caso de error
-                }
-            );
-        }
-    }
-
-  mostrarMensaje(mensaje: string){
-    this.mensajeModal = mensaje;
-    this.mostrarMensajeModal = true;
-  }
-
-  cerrarModal(){
-    this.mostrarMensajeModal=false;
-    this.visible=false;
-  }
-  cerrarModalMensaje(){
-    this.mostrarMensajeModal=false;
-  }
-
-  VerCrearRegistro(){
-    this.resetFormulario();
-    this.selectedBanco = null;
-    this.visible = true;
-  }
-  VerActualizarRegistro(banco:Banco){
-    // Cargar datos seleccionados en el formulario
-    this.formBanco.patchValue(banco);
-    this.selectedBanco = banco; // Set the selected bank manually
-    this.visible = true; // Mostrar el modal de edición
-  }
-
-
-  selectBanco(banco:Banco){
-    this.selectedBanco = banco;
-  };
-  onFilter(filterValue: string) {
-    if(filterValue){
-        const searchTerm = filterValue.toLowerCase();
-      this.bancos = this.originalBancos.filter(b => {
-        const nombre = b.ban01Descripcion.toLowerCase();
-        const codigo = b.ban01IdBanco.toLowerCase();
-        return nombre.includes(searchTerm) || codigo.includes(searchTerm);
-      });
-    }else{
-      this.bancos=[...this.originalBancos];
+        error: (err) => {
+          console.error('Error al guardar:', err);
+          this.mS.add({ severity: 'error', summary: 'Error', detail: 'No se pudo guardar el registro' });
+        },
+      })
     }
   }
+  onCancel() {
+    this.isEditing = false;
+    this.isNew = false;
+    this.bancoForm.reset({ ban01Empresa: '01' });
+  }
 
-  resetFormulario() {
-    this.formBanco.reset(); // Limpia el formulario
-    this.selectedBanco = null; // Limpia la selección
-    this.visible = false; // Asegura que el modal se cierre
-    }
-
-
-  /*VerCuentaBancaria(){
-
-    this.displayDialog = true;
-
-  }*/
-
-
-
-
+  onDelete(banco: Banco, index: number) {
+    this.confirmationService.confirm({
+      message: `¿Está seguro que desea eliminar el año <b>${banco.ban01Descripcion}</b>?`,
+      header: 'Confirmar Eliminación',
+      icon: 'pi pi-exclamation-triangle',
+      acceptLabel: 'Sí, eliminar',
+      rejectLabel: 'No, cancelar',
+      acceptButtonStyleClass: 'p-button-danger',
+      rejectButtonStyleClass: 'p-button',
+      accept: () => {
+        this.bancoService.EliminarBanco(banco.ban01Empresa, banco.ban01IdBanco).subscribe({
+          next: () => {
+            this.bancoList.splice(index, 1);
+            this.mS.add({
+              severity: 'success',
+              summary: 'Éxito',
+              detail: 'Registro eliminado'
+            });
+            this.cargarBancos()
+          }
+        })
+      }
+    })
+  }
 }
