92e71f3674172162999693171bd47db83409ad5e
AntonyFBV <antonybarreto06@gmail.com>
2025-02-18 16:25:40 -0500

    editar 18022025

--- src/app/demo/components/presupuesto/detallepresupuesto/detallepresupuesto.component.ts
+++ src/app/demo/components/presupuesto/detallepresupuesto/detallepresupuesto.component.ts
@@ -14,12 +14,13 @@ import { ToastModule } from 'primeng/toast';
 import { BreadcrumbService } from 'src/app/demo/service/breadcrumb.service';
 import { Detallepresupuesto } from '../presupuesto';
 import { PresupuestoService } from 'src/app/demo/service/presupuesto.service';
-
+import { InputNumberModule } from 'primeng/inputnumber';
+import { GlobalService } from 'src/app/demo/service/global.service';
 
 @Component({
     selector: 'app-detallepresupuesto',
     standalone: true,
-    imports: [BreadcrumbModule, RouterModule, ToastModule, ConfirmDialogModule, TableModule, PanelModule, CalendarModule, InputTextModule, ButtonModule, CommonModule, FormsModule],
+    imports: [BreadcrumbModule, RouterModule, ToastModule, ConfirmDialogModule, TableModule, PanelModule, CalendarModule, InputTextModule, InputNumberModule,ButtonModule, CommonModule, FormsModule],
     templateUrl: './detallepresupuesto.component.html',
     styleUrl: './detallepresupuesto.component.css',
     providers: [MessageService,DatePipe]
@@ -36,7 +37,12 @@ export class DetallepresupuestoComponent implements OnInit {
     groupTotals:any[] = [];
     load: boolean = false;
 
-    constructor(private messageService:MessageService,private presupuestoservice: PresupuestoService,private bs: BreadcrumbService, private router: Router, private ms: MessageService,private datePipe: DatePipe) {
+    //variables de edición
+    editingRow: Detallepresupuesto | null = null;
+    isAnyRowEditing: boolean = false;
+    editingIndex: number | null = null; // Índice de la fila en edición
+
+    constructor(private gS:GlobalService,private messageService:MessageService,private presupuestoservice: PresupuestoService,private bs: BreadcrumbService, private router: Router, private ms: MessageService,private datePipe: DatePipe) {
         const navigation = router.getCurrentNavigation();
         if (navigation?.extras?.state) {
             this.navigationData = navigation.extras.state;
@@ -111,6 +117,7 @@ export class DetallepresupuestoComponent implements OnInit {
                     totalMontoPagarSoles: 0,
                     totalMontoPagarDolares: 0,
                     totalImporte: 0,
+                    totalRetencion: 0,
                     totalPercepcion: 0,
                     totalNetoPagarSoles: 0,
                     totalNetoPagarDolares: 0,
@@ -122,6 +129,7 @@ export class DetallepresupuestoComponent implements OnInit {
             acc[item.ban02Ruc].totalMontoPagarSoles += item.ban02PagoSoles || 0;
             acc[item.ban02Ruc].totalMontoPagarDolares += item.ban02PagoDolares || 0;
             acc[item.ban02Ruc].totalImporte += item.ban02ImporteDetraccionSoles || 0;
+            acc[item.ban02Ruc].totalRetencion += item.ban02ImporteRetencionSoles || 0;
             acc[item.ban02Ruc].totalPercepcion += item.ban02ImporteSolesPercepcion || 0;
             acc[item.ban02Ruc].totalNetoPagarSoles += item.ban02SolesNeto || 0;
             acc[item.ban02Ruc].totalNetoPagarDolares += item.ban02DolaresNeto || 0;
@@ -153,6 +161,93 @@ export class DetallepresupuestoComponent implements OnInit {
         console.log(navigationExtras.state)
         this.router.navigate(['/Home/nuevo-presupuesto'], navigationExtras);
     }
+    startEditing(detalle: Detallepresupuesto, index: number) {
+        if (this.isAnyRowEditing) {
+            this.messageService.add({
+                severity: 'warn',
+                summary: 'Advertencia',
+                detail: 'Termina la edición actual antes de editar otra fila.'
+            });
+            return;
+        }
+        this.editingRow = { ...detalle }; // Copia los datos para editar
+        this.editingIndex = index; // Guarda el índice de la fila
+        this.isAnyRowEditing = true;
+    }
+
+    cancelEditing() {
+        this.editingRow = null;
+        this.editingIndex = null;
+        this.isAnyRowEditing = false;
+    }
+
+    saveEditing() {
+        if (this.editingRow && this.editingIndex !== null) {
+            const payload = this.buildBackendPayload(this.editingRow);
+            this.presupuestoservice.actualizarDetallePresupuesto(payload).subscribe({
+                next: (response) => {
+                    // Actualiza la fila en la lista original
+                    this.DetallePago[this.editingIndex] = { ...this.editingRow };
+                    this.messageService.add({
+                        severity: 'success',
+                        summary: 'Éxito',
+                        detail: 'Detalle actualizado correctamente'
+                    });
+                    this.cancelEditing();
+                },
+                error: (error) => {
+                    this.messageService.add({
+                        severity: 'error',
+                        summary: 'Error',
+                        detail: 'Error al actualizar: ' + error.message
+                    });
+                }
+            });
+        }
+    }
+
+    buildBackendPayload(detalle: Detallepresupuesto): any {
+        return {
+            ban02Empresa: this.gS.getCodigoEmpresa(),
+            ban02Ruc: detalle.ban02Ruc,
+            ban02Tipodoc: detalle.nombreTipoDocumento || "01",
+            ban02NroDoc: detalle.ban02NroDoc,
+            ban02Codigo: detalle.ban02Codigo || "COD001",
+            razonSocial: detalle.razonsocial,
+            nombreMoneda: detalle.nombremoneda,
+            nombreTipoDocumento: detalle.nombreTipoDocumento,
+            ban02Numero: this.pagnro || "NUM001",
+            ban02Fecha: this.datePipe.transform(new Date(), 'dd/MM/yyyy'),
+            ban02TipoCambio: 0,
+            ban02TipoAplic: "01",
+            ban02Moneda: detalle.ban02Moneda || "PEN",
+            ban02Soles: detalle.ban02Soles,
+            ban02Dolares: detalle.ban02Dolares,
+            ban02SolesVale: 0,
+            ban02DolaresVale: 0,
+            ban02Concepto: detalle.razonsocial,
+            ban02GiroOrden: "No se",
+            ban02BcoLiquidacion: "00",
+            ban02Redondeo: "0",
+            ban02Usuario: this.gS.getNombreUsuario(),
+            ban02Pc: "PC001",
+            ban02FechaRegistro: this.datePipe.transform(new Date(), 'yyyy-MM-dd'),
+            ban02Estado: "ElabP",
+            ban02EstadoTemp: "P",
+            ban02pagosoles: detalle.ban02PagoSoles,
+            ban02PagoDolares: detalle.ban02PagoDolares,
+            ban02TasaDetraccion: detalle.ban02Tasadetraccion,
+            ban02ImporteDetraccionSoles: detalle.ban02ImporteDetraccionSoles,
+            ban02ImporteDetraccionDolares: detalle.ban02ImporteDetraccionDolares,
+            ban02TasaRetencion: detalle.ban02TasaRetencion,
+            ban02ImporteRetencionSoles:  detalle.ban02ImporteRetencionSoles,
+            ban02ImporteRetencionDolares: detalle.ban02ImporteRetencionDolares,
+            ban02TasaPercepcion: detalle.ban02TasaPercepcion,
+            ban02ImportePercepcionSoles: detalle.ban02ImportePercepcionSoles,
+            ban02ImportePercepcionDolares: detalle.ban02ImportePercepcionDolares
+        };
+    }
+
 
 
 
