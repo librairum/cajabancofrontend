93cdd15b1b4218b73dc052c907349d5aae6586e1
librairum <ivan.atanacio@gmail.com>
2025-08-20 19:48:02 -0500

    actualizacioN

---
+++ src/app/demo/components/detraccion/detraccion-individual-ayuda/detraccion-individual-ayuda.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detraccion-individual-ayuda',
+  standalone: true,
+  imports: [],
+  templateUrl: './detraccion-individual-ayuda.component.html',
+  styleUrl: './detraccion-individual-ayuda.component.css'
+})
+export class DetraccionIndividualAyudaComponent {
+
+}
